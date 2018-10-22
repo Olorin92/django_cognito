ba83d2d1e567f5c7a3ca80cb29b9678332e3edcd
olorin92 <alex.plant@jana.com.au>
2018-10-22 16:52:32 +1100

    More cleanup around the structure and naming of the project

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_cognito',
-    version='1.1.0',
+    version='1.0.0',
     packages=find_packages(),
     url='https://github.com/Olorin92/django_cognito',
     license='',
     author='Alex Plant',
     author_email='alex.c.plant@gmail.com',
-    description='Library for allowing AWS Cognito authentication implementation in Django projects'
+    description='Library for allowing the use of AWS Cognito security in Django projects'
 )
