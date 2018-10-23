6926e3b247f36aa47ce839d5a221d449941344a4
olorin92 <alex.plant@jana.com.au>
2018-10-23 16:36:26 +1100

    Changes to support older Django versions

--- django_cognito/urls.py
+++ django_cognito/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.conf.urls import url
+from django.urls import include
 
 urlpatterns = [
     # Exclude as not appropriate for this app?
     # path('admin/', admin.site.urls),
-    path('auth/', include('django_cognito.authentication.urls')),
+    url(r'^auth/', include('django_cognito.authentication.urls')),
 ]
