6e2029a23029ca49921f65e3d03b1500a5f038af
olorin92 <alex.plant@jana.com.au>
2018-10-31 17:03:02 +1100

    More cleaning up of unneeded dependencies

--- setup.py
+++ setup.py
@@ -11,11 +11,15 @@ setup(
     author_email='alex.c.plant@gmail.com',
     description='Library for allowing the use of AWS Cognito security in Django projects',
     install_requires=[
+        "asn1crypto==0.24.0",
         "boto3==1.9.0",
         "botocore==1.12.0",
-        "Django>=1.10.0",
+        "cryptography==2.3.1",
+        "Django==2.1.2",
         "djangorestframework==3.8.2",
+        "idna==2.7",
+        "PyJWT==1.6.4",
+        "python-dateutil==2.7.3",
         "urllib3==1.23",
-        "jwt==0.5.4"
     ]
 )
