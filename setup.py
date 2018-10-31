442d569301650808e7125e04ad8bfb07562d6c55
olorin92 <alex.plant@jana.com.au>
2018-10-31 16:43:50 +1100

    Try and make setup requirements minimal

--- setup.py
+++ setup.py
@@ -11,22 +11,11 @@ setup(
     author_email='alex.c.plant@gmail.com',
     description='Library for allowing the use of AWS Cognito security in Django projects',
     install_requires=[
-        "asn1crypto==0.24.0",
         "boto3==1.9.0",
         "botocore==1.12.0",
-        "cffi==1.11.5",
-        "cryptography==2.3.1",
         "Django>=1.10.0",
         "djangorestframework==3.8.2",
-        "docutils==0.14",
-        "idna==2.7",
-        "jmespath==0.9.3",
-        "pycparser==2.18",
-        "PyJWT==1.6.4",
-        "python-dateutil==2.7.3",
-        "pytz==2018.5",
-        "s3transfer==0.1.13",
-        "six==1.11.0",
         "urllib3==1.23",
+        "jwt==1.6.4"
     ]
 )
